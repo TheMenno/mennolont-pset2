ed3b51aad7750ec0ebb4639cb47216cb4d6eace7
TheMenno <menno_lont@live.nl>
2017-09-14 12:01:59 +0200

    Story.java finally does things

--- app/src/main/java/com/example/menno_000/mennolont_pset2/Activity2.java
+++ app/src/main/java/com/example/menno_000/mennolont_pset2/Activity2.java
@@ -1,12 +1,19 @@
 package com.example.menno_000.mennolont_pset2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Activity2 extends AppCompatActivity {
+
+    Story story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,32 @@ public class Activity2 extends AppCompatActivity {
 
         startActivity(intent);
         finish();
+    }
+
+
+    public void storytell() {
+
+        Context context = getApplicationContext();
+
+        try { InputStream stream = context.getAssets().open("madlib0_simple.txt");
+            story = new Story(stream);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // First make clear that everything is set to standard values
+        story.clear();
+
+        // Then check if there are more words to fill in, and else return the story
+        while (story.isFilledIn() == false) {
+            String next = story.getNextPlaceholder();
+            story.fillInPlaceholder(next);
+        }
+
+        // Story
+        story.toString();
+
     }
 }
